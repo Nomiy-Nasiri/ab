9824fee2792832f9030b0fcb98a66d930687808b
Nomiy-Nasiri <nomannasiri5@gmail.com>
2022-11-18 21:06:28 +0500

    modify header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <p>hello</p>
+      <p>hellolhs;ldhflhsd;gohlskhgh;kldfhg/sd;khg;sljshdglajhg;;lhg</p>
     </div>
   )
 }
